1b7efae833155f200177d4d0cedbee8621335053
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2025-10-08 14:32:00 +0530

    Operators

--- binary_operator.java
+++ binary_operator.java
@@ -9,9 +9,14 @@ public class binary_operator {
         return a | b;
     }
 
+    public static int binaryXOR(int a, int b) {
+        return a ^ b;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bianry And: " + binaryAND(5, 6));
-        System.out.println("Bianry OR: " + binaryOR(5, 6));
+        System.out.println("Binary And: " + binaryAND(5, 6));
+        System.out.println("Binary OR: " + binaryOR(5, 6));
+        System.out.println("Binary XOR: " + binaryXOR(5, 6));
     }
 }
