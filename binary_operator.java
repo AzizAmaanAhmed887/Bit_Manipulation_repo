89b6df1564229d297d89e1296b05adf449158a8d
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2025-10-08 14:55:37 +0530

    Operators

--- binary_operator.java
+++ binary_operator.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class binary_operator {
     public static int binaryAND(int a, int b) {
         return a & b;
@@ -13,10 +11,14 @@ public class binary_operator {
         return a ^ b;
     }
 
+    public static int binaryOnesComplement(int a) {
+        return ~a;
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Binary And: " + binaryAND(5, 6));
-        System.out.println("Binary OR: " + binaryOR(5, 6));
-        System.out.println("Binary XOR: " + binaryXOR(5, 6));
+        System.out.println("Binary One's Complement: " + binaryOnesComplement(0));
+//        System.out.println("Binary And: " + binaryAND(5, 6));
+//        System.out.println("Binary OR: " + binaryOR(5, 6));
+//        System.out.println("Binary XOR: " + binaryXOR(5, 6));
     }
 }
